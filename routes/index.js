ea960508e1a08ca537f9feecddbc9061f94186ce
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2023-07-13 22:59:36 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const http = require("http");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', (req, res) => {
+
+  const host = 'localhost'; 
+  const port = 8000;
+
+  const requestListener = function (req, res) {
+    res.writeHead(200);
+    
+  };
+  
+
+  const server = http.createServer(requestListener);
+  server.listen(port, host, () => {
+
+      console.log(`Server is running on http://${host}:${port}`);
+
+      const cport = req.connection.remotePort
+      const cadress = req.connection.remoteAddress
+      let c_port = cport.toString();
+      res.render('index', {ip:cadress, port:c_port});
+
+      req.on('close', () => {
+        console.log('bağlantı koptu');
+        server.close()
+      });
+
+  });
+
 });
+
 
 module.exports = router;
