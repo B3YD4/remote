8475891864d77f4645f22b5863006cc72f124694
B3YD4 <41930266+B3YD4@users.noreply.github.com>
2023-07-13 22:48:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,22 +39,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-
-const host = 'localhost'; 
-const port = 8000;
-
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    const cport = req.connection.remotePort
-    const cadress = req.connection.remoteAddress
-    let c_port = cport.toString();
-    res.write("Sisteme Hoşgeldin Bağlantı Adresin\n" + cadress + ' : ' + c_port);
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`); 
-}); 
-
 module.exports = app;
